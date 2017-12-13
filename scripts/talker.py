d4a9ae0f6c5174b06f9e4a8ba79ac110a9f88226
lianbo2006 <513748889@qq.com>
2017-12-13 13:29:09 +0800

    Initial commit

---
+++ scripts/talker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    num = 0
+    while not rospy.is_shutdown():
+        # hello_str = "hello world %s" % rospy.get_time()
+        num = num + 1
+        num_str = "{:0>4d}".format(num)
+        hello_str = "hello world " + num_str + " %s"% rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
