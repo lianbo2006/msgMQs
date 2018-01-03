63a406d578a2a0b3527c8da58ef03791c1a891e2
lianbo2006 <513748889@qq.com>
2018-01-03 22:56:38 +0800

    some comment

--- Rabbit_test/工作队列/new_task.py
+++ Rabbit_test/工作队列/new_task.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+#-*-coding:utf-8-*-
 import pika
 import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+channel.queue_declare(queue='task_queue', durable=True) #durable=True确保task_queue队列持久化
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
+message = ' '.join(sys.argv[1:]) or "Hello World!" #可以读取系统输入的参数，通过加.来延长处理时间
 channel.basic_publish(exchange='',
                       routing_key='task_queue',
                       body=message,
                       properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
+                         delivery_mode = 2, # make message persistent 让消息持久化
                       ))
 print " [x] Sent %r" % (message,)
 connection.close()
--- Rabbit_test/工作队列/worker.py
+++ Rabbit_test/工作队列/worker.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
+#-*-coding:utf-8-*-
 import pika
 import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+channel.queue_declare(queue='task_queue', durable=True) #durable=True确保task_queue队列持久化
 print ' [*] Waiting for messages. To exit press CTRL+C'
 
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
     time.sleep( body.count('.') )
     print " [x] Done"
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag = method.delivery_tag) #发送消息确认，消息队列收到后会删除对应的消息
 
-channel.basic_qos(prefetch_count=1)
+channel.basic_qos(prefetch_count=1) #保证消息公平调度
 channel.basic_consume(callback,
                       queue='task_queue')
 
